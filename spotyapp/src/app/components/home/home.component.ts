4704dd31ddc07d465389896ce9580f8dae7924a6
Matias Candella <matiascandella@hotmail.com>
2024-01-23 23:41:37 -0300

    first commit

---
+++ spotyapp/src/app/components/home/home.component.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html'
+})
+export class HomeComponent implements OnInit {
+  
+
+  constructor( private http: HttpClient ){
+    
+  }
+
+  ngOnInit() {
+    
+  }
+}
