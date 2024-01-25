e5481fdc1092267596cbe0f4a73353ee73486ee8
Matias Candella <matiascandella@hotmail.com>
2024-01-25 15:05:29 -0300

    Cambio pequeño en getNewReleases

--- spotyapp/src/app/services/spotify.service.ts
+++ spotyapp/src/app/services/spotify.service.ts
@@ -2,7 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root' //forma automatica de importar servicios, sin necesidad de incorporarlos en el app.module.ts
+  providedIn: 'root' //Forma automatica de importar servicios, sin necesidad de incorporarlos en el app.module.ts
 })
 export class SpotifyService {
 
@@ -11,7 +11,7 @@ export class SpotifyService {
 
   getNewReleases() {
     const headers = new HttpHeaders({
-      'Authorization': 'Bearer BQDUBzuOo5npTBqraQJuFJwb---lsqND9tm0btM8GtLPO0OHD-H7-qqZJs1d64VHphzYkUxCZ0kvZKcx8_-G-FDnjpNo2TnX4XDiHtQ2GWnq0ZpKpvg'
+      'Authorization': 'Bearer ' //Agregar token
     });
     this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=20', {headers})
     .subscribe( data => {
